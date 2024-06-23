ca1e7480ecbfb5d9a706fb0ca69a66d6e71a92ec
Michael Hamilton <28913158+MicHamoNZ@users.noreply.github.com>
2024-06-23 19:54:38 +1200

    home testing UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,45 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Budget from '../components/Envelopes';
 
 export function Home() {
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-    fetch('/envelopes')
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-  }, []);
-
   return (
-    <>
-      <div className='App'>
-        <header className='App-header'>
-          <h2>Home page</h2>
-          <p>
-            {typeof backendData.data === 'undefined' ? (
-              <p>Loading...</p>
-            ) : (
-              <div>
-                <table>
-                  <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Budget</th>
-                  </tr>
-                  {backendData.data.map((key) => (
-                    <tr>
-                      <td>{key.id}</td>
-                      <td>{key.envelope_name}</td>
-                      <td>{key.budget}</td>
-                    </tr>
-                  ))}
-                </table>
-              </div>
-            )}
-          </p>
-        </header>
-      </div>
-    </>
+    <Container>
+      <h1>Your budget balances:</h1>
+      <Budget />
+    </Container>
   );
 }
